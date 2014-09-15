384cd52fda3c3e11cbee05d7c8f8cc83a3b3465e
Madhav Abhyankar <madhavsemail@gmail.com>
2014-09-15 15:32:16 -0500

    Removed Unnecessary Code

--- RoadTrip.Web/app/controllers/addNewRoadTripController.js
+++ RoadTrip.Web/app/controllers/addNewRoadTripController.js
@@ -1,12 +1,24 @@
 ﻿'use strict';
 
 app.controller('addNewRoadTripController', [
-    '$scope', 'roadTripService', function ($scope, roadTripService) {
+    '$scope', '$location', 'roadTripService', function ($scope, $location, roadTripService) {
         $scope.newRoadTrip = {}
 
         $scope.AddNewRoadTrip = function() {
             roadTripService.addNewRoadTrip($scope.newRoadTrip);
-
+            $location.path('/myroadtrips');
         }
+
+        var getHasCodeForTrip = function () {
+            var seed = new Date().toString();
+            var hash = 0, i, chr, len;
+            if (this.length == 0) return hash;
+            for (i = 0, len = this.length; i < len; i++) {
+                chr = this.charCodeAt(i);
+                hash = ((hash << 5) - hash) + chr;
+                hash |= 0; // Convert to 32bit integer
+            }
+            return hash;
+        };
     }
 ]);
