6a2c90cc7b95393311729aa6c36a4138756cb2bd
Madhav Abhyankar <madhavsemail@gmail.com>
2014-09-15 15:36:55 -0500

    hash

--- RoadTrip.Web/app/controllers/addNewRoadTripController.js
+++ RoadTrip.Web/app/controllers/addNewRoadTripController.js
@@ -3,22 +3,34 @@
 app.controller('addNewRoadTripController', [
     '$scope', '$location', 'roadTripService', function ($scope, $location, roadTripService) {
         $scope.newRoadTrip = {}
-
-        $scope.AddNewRoadTrip = function() {
+        
+        $scope.AddNewRoadTrip = function () {
+            
             roadTripService.addNewRoadTrip($scope.newRoadTrip);
             $location.path('/myroadtrips');
         }
 
+        function generatePassword() {
+            var length = 8,
+                charset = "abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+                retVal = "";
+            for (var i = 0, n = charset.length; i < length; ++i) {
+                retVal += charset.charAt(Math.floor(Math.random() * n));
+            }
+            return retVal;
+        }
         var getHasCodeForTrip = function () {
-            var seed = new Date().toString();
+            var seed = new Date().toString() + generatePassword();
             var hash = 0, i, chr, len;
-            if (this.length == 0) return hash;
-            for (i = 0, len = this.length; i < len; i++) {
+            if (seed.length == 0) return hash;
+            for (i = 0, len = seed.length; i < len; i++) {
                 chr = this.charCodeAt(i);
                 hash = ((hash << 5) - hash) + chr;
                 hash |= 0; // Convert to 32bit integer
             }
             return hash;
         };
+        
+        alert(getHasCodeForTrip());
     }
 ]);
