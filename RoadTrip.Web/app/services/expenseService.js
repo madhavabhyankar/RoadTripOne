9d8e811667ac26eaf316b6c18c1e045d305feb2b
Madhav Abhyankar <madhavsemail@gmail.com>
2014-09-16 15:36:45 -0500

    Expense update 2

--- RoadTrip.Web/app/services/expenseService.js
+++ RoadTrip.Web/app/services/expenseService.js
@@ -2,18 +2,34 @@
     '$http', '$q', 'ngAuthSettings',
         function ($http, $q, ngAuthSettings) {
             var serviceBase = ngAuthSettings.apiServiceBaseUri;
+            var allExpenses = [];
             var _getAllExpenses = function(roadTripId) {
                 var deferred = $q.defer();
-                $http.get(serviceBase + 'api/Expense/GetExpensesForTrip/' + roadTripId).then(
-                    function(data) {
-
-                        deferred.resolve(data.data);
-                    }, function(e) {
-                        deferred.reject(e);
-                    });
+                if (allExpenses.length > 0) {
+                    deferred.resolve(allExpenses);
+                } else {
+                    $http.get(serviceBase + 'api/Expense/GetExpensesForTrip/' + roadTripId).then(
+                        function(data) {
+                            allExpenses = data.data;
+                            deferred.resolve(data.data);
+                        }, function(e) {
+                            deferred.reject(e);
+                        });
+                }
                 return deferred.promise;
             }
+            var _addExpense = function(expenseToAdd) {
+                var deferred = $q.defer();
 
+                $http.post(serviceBase + 'api/Expense/AddExpenseToTrip', expenseToAdd).then(
+                    function(data) {
+
+                    }, function(e) {
+
+                    });
+
+                return deferred.promise;
+            }
             return {
                 getAllExpenses: _getAllExpenses
             };
